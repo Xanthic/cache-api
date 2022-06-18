8ec187e6bd7ceb16fca975f44e67399e0bc8f037
Sidd <iProdigy@users.noreply.github.com>
2022-06-18 01:21:35 -0500

    feat: add more checks to CacheApiSpec#validate

--- src/main/java/io/github/iprodigy/cache/CacheApiSpec.java
+++ src/main/java/io/github/iprodigy/cache/CacheApiSpec.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
@@ -13,6 +14,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.function.Consumer;
 
 @Data
+@Slf4j
 @Accessors(fluent = true)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class CacheApiSpec<K, V> implements ICacheSpec<K, V> {
@@ -25,17 +27,19 @@ public final class CacheApiSpec<K, V> implements ICacheSpec<K, V> {
 	}
 
 	/**
-	 * validate the config
-	 * <p>
-	 * TODO: proper validation
+	 * Ensure the config is valid
 	 */
 	public void validate() {
 		Objects.requireNonNull(provider, "provider may not be null!");
-		Objects.requireNonNull(maxSize, "maxSize may not be null!");
-		// Objects.requireNonNull(expiryTime, "expiryTime may not be null!");
-		// Objects.requireNonNull(expiryType, "expiryType may not be null!");
-		// Objects.requireNonNull(removalListener, "removalListener may not be null!");
-		// Objects.requireNonNull(executor, "executor may not be null!");
+
+		if (maxSize != null && maxSize < 0)
+			throw new MisconfiguredCacheException("maxSize may not be negative!");
+
+		if (expiryTime != null && expiryTime.isNegative())
+			throw new MisconfiguredCacheException("expiryTime may not be negative!");
+
+		if (expiryTime != null && expiryType == null)
+			log.warn("Cache specification enables expiry time but does not specify ExpiryType");
 	}
 
 	private CacheProvider provider = CacheApiSettings.getInstance().getDefaultCacheProvider();
