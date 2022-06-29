9deb6e12b1cc99cb61c3fe91b9b55dbcb40780c4
Sidd <iProdigy@users.noreply.github.com>
2022-06-29 15:20:21 -0500

    fix: distinguish expired removals for androidx listener

--- provider-androidx/src/main/java/io/github/iprodigy/cache/provider/androidx/AndroidExpiringLruProvider.java
+++ provider-androidx/src/main/java/io/github/iprodigy/cache/provider/androidx/AndroidExpiringLruProvider.java
@@ -1,6 +1,5 @@
 package io.github.iprodigy.cache.provider.androidx;
 
-import androidx.collection.LruCache;
 import io.github.iprodigy.cache.api.Cache;
 import io.github.iprodigy.cache.api.ICacheSpec;
 import io.github.iprodigy.cache.core.AbstractCacheProvider;
@@ -15,9 +14,8 @@ public final class AndroidExpiringLruProvider extends AbstractCacheProvider {
 		ScheduledExecutorService executor = spec.executor();
 		Duration expiryTime = spec.expiryTime();
 		if (executor == null) handleUnsupportedExpiry(expiryTime);
-		LruCache<K, V> cache = AndroidLruProvider.build(spec.maxSize(), spec.removalListener());
-		if (expiryTime == null) return new LruDelegate<>(cache);
+		if (expiryTime == null) return new LruDelegate<>(AndroidLruProvider.build(spec.maxSize(), spec.removalListener()));
 		ScheduledExecutorService exec = executor != null ? executor : Executors.newSingleThreadScheduledExecutor();
-		return new ExpiringLruDelegate<>(cache, expiryTime.toNanos(), getExpiryType(spec.expiryType()), exec);
+		return new ExpiringLruDelegate<>(spec.maxSize(), spec.removalListener(), expiryTime.toNanos(), getExpiryType(spec.expiryType()), exec);
 	}
 }
