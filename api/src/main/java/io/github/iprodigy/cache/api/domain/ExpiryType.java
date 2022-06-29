367c8e03ef953f8ca33b2d00b4da3e9634faa1c8
Sidd <iProdigy@users.noreply.github.com>
2022-06-29 02:07:22 -0500

    docs: add api module javadocs

--- api/src/main/java/io/github/iprodigy/cache/api/domain/ExpiryType.java
+++ api/src/main/java/io/github/iprodigy/cache/api/domain/ExpiryType.java
@@ -1,6 +1,18 @@
 package io.github.iprodigy.cache.api.domain;
 
+/**
+ * The type of expiration policy to use.
+ */
 public enum ExpiryType {
+
+	/**
+	 * Entries should expire when no accesses occur during a specific time period.
+	 */
 	POST_ACCESS,
+
+	/**
+	 * Entries should expire after a specific time period has passed since the entry creation.
+	 */
 	POST_WRITE
+
 }
