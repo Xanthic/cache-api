e2ca29f96d4db5f8b7d2dd97ed70a817bc810caf
iProdigy <8106344+iProdigy@users.noreply.github.com>
2023-10-20 20:44:52 -0700

    chore: build with 2.16 rc1
    Signed-off-by: iProdigy <8106344+iProdigy@users.noreply.github.com>

--- jackson/build.gradle.kts
+++ jackson/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     api(project(":cache-core"))
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2") // TODO: 2.16.0
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0-rc1")
 }
 
 publishing.publications.withType<MavenPublication> {
