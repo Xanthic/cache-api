7c9e962fcf58253f26ee64581ee93460dc7de04f
iProdigy <gitprodigy@proton.me>
2023-10-20 21:57:21 -0700

    chore: declare rich version

--- jackson/build.gradle.kts
+++ jackson/build.gradle.kts
@@ -1,6 +1,10 @@
 dependencies {
     api(project(":cache-core"))
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0-rc1")
+    implementation("com.fasterxml.jackson.core:jackson-databind") {
+        version {
+            require("2.16.0-rc1") // imposes a lower bound on acceptable versions
+        }
+    }
 }
 
 publishing.publications.withType<MavenPublication> {
