5b5083d0e243ae7aaf9b6d77c25400d08678b3fa
iProdigy <gitprodigy@proton.me>
2023-10-22 20:38:28 -0700

    chore: simplify tests

--- jackson/src/test/java/io/github/xanthic/jackson/XanthicJacksonCacheProviderTest.java
+++ jackson/src/test/java/io/github/xanthic/jackson/XanthicJacksonCacheProviderTest.java
@@ -8,8 +8,6 @@ import com.fasterxml.jackson.databind.deser.DeserializerCache;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.ser.SerializerCache;
 import com.fasterxml.jackson.databind.type.TypeFactory;
-import io.github.xanthic.cache.provider.caffeine.CaffeineProvider;
-import io.github.xanthic.jackson.util.TrackedCache;
 import io.github.xanthic.jackson.util.TrackedCacheProvider;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -39,44 +37,44 @@ class XanthicJacksonCacheProviderTest {
 
 	@Test
 	void deserialize() throws JsonProcessingException {
-		TrackedCacheProvider provider = new TrackedCacheProvider(new CaffeineProvider());
+		TrackedCacheProvider provider = new TrackedCacheProvider();
 		ObjectMapper mapper = JsonMapper.builder()
 			.cacheProvider(createCacheProvider(provider))
 			.build();
-		assertFalse(provider.getConstructedCaches().stream().anyMatch(TrackedCache::hasInteraction));
+		assertFalse(provider.getConstructedCaches().stream().anyMatch(c -> c.size() > 0));
 		Foo foo = mapper.readValue("{\"bar\":\"baz\"}", Foo.class);
 		assertNotNull(foo);
 		assertEquals("baz", foo.getBar());
-		assertTrue(provider.getConstructedCaches().stream().anyMatch(TrackedCache::hasInteraction));
+		assertTrue(provider.getConstructedCaches().stream().anyMatch(c -> c.size() > 0));
 	}
 
 	@Test
 	void serialize() throws JsonProcessingException {
-		TrackedCacheProvider provider = new TrackedCacheProvider(new CaffeineProvider());
+		TrackedCacheProvider provider = new TrackedCacheProvider();
 		ObjectMapper mapper = JsonMapper.builder()
 			.cacheProvider(createCacheProvider(provider))
 			.build();
-		assertFalse(provider.getConstructedCaches().stream().anyMatch(TrackedCache::hasInteraction));
+		assertFalse(provider.getConstructedCaches().stream().anyMatch(c -> c.size() > 0));
 		String json = mapper.writeValueAsString(new Foo("baz"));
 		assertEquals("{\"bar\":\"baz\"}", json);
-		assertTrue(provider.getConstructedCaches().stream().anyMatch(TrackedCache::hasInteraction));
+		assertTrue(provider.getConstructedCaches().stream().anyMatch(c -> c.size() > 0));
 	}
 
 	@Test
 	void constructType() {
-		TrackedCacheProvider provider = new TrackedCacheProvider(new CaffeineProvider());
+		TrackedCacheProvider provider = new TrackedCacheProvider();
 		ObjectMapper mapper = JsonMapper.builder()
 			.cacheProvider(createCacheProvider(provider))
 			.build();
-		assertFalse(provider.getConstructedCaches().stream().anyMatch(TrackedCache::hasInteraction));
+		assertFalse(provider.getConstructedCaches().stream().anyMatch(c -> c.size() > 0));
 		JavaType type = mapper.getTypeFactory().constructParametricType(List.class, Integer.class);
 		assertNotNull(type);
-		assertTrue(provider.getConstructedCaches().stream().anyMatch(TrackedCache::hasInteraction));
+		assertTrue(provider.getConstructedCaches().stream().anyMatch(c -> c.size() > 0));
 	}
 
 	@Test
 	void constructMultiple() throws JsonProcessingException {
-		TrackedCacheProvider provider = new TrackedCacheProvider(new CaffeineProvider());
+		TrackedCacheProvider provider = new TrackedCacheProvider();
 		assertEquals(0, provider.getConstructedCaches().size());
 
 		ObjectMapper m1 = JsonMapper.builder().cacheProvider(createCacheProvider(provider)).build();
@@ -84,12 +82,14 @@ class XanthicJacksonCacheProviderTest {
 		m1.writeValueAsString(new Foo("baz"));
 		m1.getTypeFactory().constructParametricType(List.class, Integer.class);
 		assertEquals(3, provider.getConstructedCaches().size());
+		assertTrue(provider.getConstructedCaches().stream().allMatch(c -> c.size() > 0));
 
 		ObjectMapper m2 = JsonMapper.builder().cacheProvider(createCacheProvider(provider)).build();
 		m2.readValue("{\"bar\":\"baz\"}", Foo.class);
 		m2.writeValueAsString(new Foo("baz"));
 		m2.getTypeFactory().constructParametricType(List.class, Integer.class);
 		assertEquals(6, provider.getConstructedCaches().size());
+		assertTrue(provider.getConstructedCaches().stream().allMatch(c -> c.size() > 0));
 	}
 
 	private static CacheProvider createCacheProvider(TrackedCacheProvider trackedProvider) {
--- jackson/src/test/java/io/github/xanthic/jackson/util/TrackedCacheProvider.java
+++ jackson/src/test/java/io/github/xanthic/jackson/util/TrackedCacheProvider.java
@@ -3,6 +3,7 @@ package io.github.xanthic.jackson.util;
 import io.github.xanthic.cache.api.Cache;
 import io.github.xanthic.cache.api.CacheProvider;
 import io.github.xanthic.cache.api.ICacheSpec;
+import io.github.xanthic.cache.core.CacheApiSettings;
 import lombok.Value;
 
 import java.util.ArrayList;
@@ -10,12 +11,12 @@ import java.util.List;
 
 @Value
 public class TrackedCacheProvider implements CacheProvider {
-	CacheProvider underlyingProvider;
-	List<TrackedCache<?, ?>> constructedCaches = new ArrayList<>();
+	CacheProvider underlyingProvider = CacheApiSettings.getInstance().getDefaultCacheProvider();
+	List<Cache<?, ?>> constructedCaches = new ArrayList<>();
 
 	@Override
 	public <K, V> Cache<K, V> build(ICacheSpec<K, V> spec) {
-		TrackedCache<K, V> cache = new TrackedCache<>(underlyingProvider.build(spec));
+		Cache<K, V> cache = underlyingProvider.build(spec);
 		constructedCaches.add(cache);
 		return cache;
 	}
