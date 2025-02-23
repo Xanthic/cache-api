307e8c9e16b8f26f2514856558c38139d4f5fab1
iProdigy <gitprodigy@proton.me>
2025-02-23 10:36:06 -0600

    chore: use junit-bom for test fixtures

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,8 +5,9 @@ plugins {
 dependencies {
     api(project(":cache-api"))
 
-    testFixturesImplementation("org.junit.jupiter:junit-jupiter:5.12.0")
-    testFixturesRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.0")
+    testFixturesImplementation(platform("org.junit:junit-bom:5.12.0"))
+    testFixturesImplementation("org.junit.jupiter:junit-jupiter")
+    testFixturesRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testFixturesImplementation("org.awaitility:awaitility:4.3.0")
 }
 
